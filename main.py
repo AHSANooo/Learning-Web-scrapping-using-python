6c5dc5533ff9030b2a9f4cea7e0972f610f82872
Muhammad AHSAN <142413686+AHSANooo@users.noreply.github.com>
2024-08-08 16:25:10 +0500

    Open in Firefox

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+import time as t
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
@@ -9,5 +9,6 @@ elem = driver.find_element(By.NAME, "q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
+t.sleep(7)
 assert "No results found." not in driver.page_source
 driver.close()
